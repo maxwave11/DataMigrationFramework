06e0f51f623883c1aea5e586a59dd87ad75939dc
maxwave <maxwave@mail.ru>
2017-03-09 16:02:59 +0300

    Enable ValidationUnit

--- src/Mapping/TransitionNodes/TransitUnits/ValidationUnit.cs
+++ src/Mapping/TransitionNodes/TransitUnits/ValidationUnit.cs
@@ -10,31 +10,30 @@ namespace XQ.DataMigration.Mapping.TransitionNodes.TransitUnits
         public string Message { get; set; }
 
         [XmlAttribute]
-        public TransitContinuation Continuation { get; set; }
+        public TransitContinuation OnInvalid { get; set; }
 
         public override TransitResult TransitValue(ValueTransitContext ctx)
         {
-            throw new NotImplementedException();
-            //var transitValue = ctx.TransitValue;
-            //var transitResult = base.TransitValueInternal(ctx);
+            var transitValue = ctx.TransitValue;
+            var transitResult = base.TransitValue(ctx);
 
-            //var boolValue = transitResult.Value as bool?;
-            //if (!boolValue.HasValue)
-            //    throw new Exception($"Result of Expression execution in {nameof(ValidationUnit)} must have Boolean type");
+            var boolValue = transitResult.Value as bool?;
+            if (!boolValue.HasValue)
+                throw new Exception($"Result of Expression execution in {nameof(ValidationUnit)} must have Boolean type");
 
-            //TransitContinuation continuation;
-            //string ruleMessage = "";
-            //if (boolValue.Value == false)
-            //{
-            //    continuation = Continuation;
-            //    var defaultMessage = $"The value={ctx.TransitValue} didn't pass validation expression {Expression}";
-            //    ruleMessage = Message ?? defaultMessage;
-            //}
-            //else
-            //{
-            //    continuation = TransitContinuation.Continue;
-            //}
-            //return new TransitResult(continuation, transitValue, ruleMessage);
+            TransitContinuation continuation;
+            string ruleMessage = "";
+            if (boolValue.Value == false)
+            {
+                continuation = OnInvalid;
+                var defaultMessage = $"The value={ctx.TransitValue} didn't pass validation expression {Expression}";
+                ruleMessage = Message ?? defaultMessage;
+            }
+            else
+            {
+                continuation = TransitContinuation.Continue;
+            }
+            return new TransitResult(continuation, transitValue, ruleMessage);
         }
     }
 }
