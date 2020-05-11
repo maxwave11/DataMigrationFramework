9950117ec186ea511d20da622a58ca5a37886043
Max <maxwave@exquance.com>
2020-05-11 19:40:02 +0300

    Allow to read yaml from stream

--- src/MapConfiguration/MapConfigReaderYaml.cs
+++ src/MapConfiguration/MapConfigReaderYaml.cs
@@ -12,43 +12,27 @@ using YamlDotNet.Serialization;
 
 namespace XQ.DataMigration.MapConfiguration
 {
-
-    class nnn : INodeTypeResolver
-    {
-        public bool Resolve(NodeEvent nodeEvent, ref Type currentType)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public class MapConfigReaderYaml
     {
-        private readonly Stream _fileStream;
-        private readonly String _fileName;
-        //private readonly List<Type> _customElements = new List<Type>();
+        private string _yaml;
 
         public MapConfigReaderYaml(string fileName)
         {
-          //  _fileStream = new FileStream(fileName, FileMode.Open);
-            _fileName = fileName;
-            //register default commonly used providers of source data
-            //RegisterDataProvider(typeof(CsvDataSource));
-            //RegisterDataProvider(typeof(ExcelDataSource));
-            //RegisterDataProvider(typeof(SqlDataSource));
+            _yaml = File.ReadAllText(fileName);
         }
 
         public MapConfigReaderYaml(Stream fileStream)
         {
-            _fileStream = fileStream;
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                _yaml = reader.ReadToEnd();
+            }
         }
 
         public MapConfig Read(IEnumerable<Type> customTypes)
         {
-            var yamlInput = File.ReadAllText(_fileName);
-
             var commandMapping = new Dictionary<string, Type>()
             {
-                // { "transit-data", typeof(TransitDataCommand) },
-                // { "transit", typeof(TransitValueCommand) },
                 { "condition", typeof(IfTransition) },
                 { "REPLACE", typeof(ReplaceTransitUnit) },
                 { "FLOW", typeof(FlowTransition) },
@@ -59,7 +43,6 @@ namespace XQ.DataMigration.MapConfiguration
                 { "GET", typeof(ReadTransitUnit) },
                 { "IF", typeof(IfTransition) },
                 { "TRACE", typeof(TraceTransitUnit) },
-
 
                 { "csv", typeof(CsvDataSource) },
                 { "excel", typeof(ExcelDataSource) },
@@ -75,10 +58,9 @@ namespace XQ.DataMigration.MapConfiguration
                 .ToList()
                 .ForEach(type => builder = builder.WithTagMapping("!" + type.Key, type.Value));
 
-            //TranslateMapConfig(yamlInput);
             var deserializer = builder.Build();
 
-            var mapConfig = deserializer.Deserialize<MapConfig>(yamlInput);
+            var mapConfig = deserializer.Deserialize<MapConfig>(_yaml);
             mapConfig.Initialize();
 
             return mapConfig;
